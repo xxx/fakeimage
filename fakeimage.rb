d0f710863809ff1843c759f9c21c9e4738699009
mpd <mpd@jesters-court.net>
2010-01-30 17:02:02 -0800

    update the documentation a bit.

--- fakeimage.rb
+++ fakeimage.rb
@@ -9,7 +9,7 @@ FORMATS = {
 }
 
 get '/' do
-  "Welcome to fakeimage. Please see the README at <a href='http://github.com/xxx/fakeimage'>http://github.com/xxx/fakeimage</a> for usage info so I don't have to maintain two copies of it. Try <a href='http://fakeimage.heroku.com/243x350.gif?color=darkorchid2&textcolor=snow'>this</a>."
+  "<p>Welcome to fakeimage.</p><p>Please see the README (specifically the 'Use' section) at <a href='http://github.com/xxx/fakeimage'>http://github.com/xxx/fakeimage</a> for usage info so I don't have a chance to let one of the copies get out of date.</p><p>Example: <img src='/243x350.gif?color=darkorchid2&textcolor=!B9AF55' /></p><p>Code: <code>&lt;img src='http://fakeimage.heroku.com/243x350.gif?color=darkorchid2&textcolor=!B9AF55' /&gt;</code>"
 end
 
 get '/:size' do
@@ -46,7 +46,7 @@ get '/:size' do
       :type => "image/#{format}"
 
   rescue Exception => e
-    "Something broke. Use this thing like http://host:port/200x300, or add color and textcolor params to decide color. Error is: [#{e}]"
+    "<p>Something broke.  You can try <a href='/200x200'>this simple test</a>. If this error occurs there as well, you are probably missing app dependencies. Make sure RMagick is installed correctly. If the test works, you are probably passing bad params in the url.</p><p>Use this thing like http://host:port/200x300, or add color and textcolor params to decide color.</p><p>Error is: [<code>#{e}</code>]</p>"
   end
 
 end
